40cbe562c7af371c6e884d2edb32d7514139a528
ii-158 <okawa@ideaimage.co.jp>
2022-02-08 12:49:04 +0900

    add style

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,13 @@
 import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
 
+import classes from "./Todos.module.css"
+
 const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
 	props
 ) => {
 	return (
-		<ul>
+		<ul className={classes.todos}>
 			{props.items.map((item) => (
 				<TodoItem
 					key={item.id}
